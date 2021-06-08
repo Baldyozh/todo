92e6d91dc49c193e61e6b964603466609dff27fc
baldyozh <nikiguy13@gmail.com>
2021-06-08 20:59:03 +0600

    xz

--- s.js
+++ s.js
@@ -1,19 +1,33 @@
-let but = document.getElementById('but')
-let container = document.getElementById('place-for-notifications')
-let containerForNotifition = '<div class="wrapper-for-notification"></div>' 
+let but = document.getElementById('but');
+let container = document.getElementById('place-for-notifications');
+let containerForNotification = document.createElement('div');
+containerForNotification.className = 'wrapper-for-note';
+let html_for_name_of_note = document.createElement('h2');
+html_for_name_of_note.className = 'name';
+let html_for_note = document.createElement('p');
+html_for_note.className = 'text-of-note';
+
 but.onclick = () => {
-    let nameOfNotification = document.getElementById('name');
-    let textOfNotification = document.getElementById('notification');
-    if (textOfNotification.value.length > 0){
-        if (nameOfNotification.value.length > 0){
-            container.append(containerForNotifition);
-            let note = container.lastChild
-            note.append('<h2 class="name-of-note">' + nameOfNotification + '</h2>')
-            note.append('<p class="text-of-note>"' + textOfNotification + '</p>')
+    let nameOfNotification = document.getElementById('name').value;
+    console.log(nameOfNotification)
+    let textOfNotification = document.getElementById('notification').value;
+    console.log(textOfNotification)
+    if (textOfNotification.length > 0){
+        if (nameOfNotification.length > 0){
+            container.append(containerForNotification);
+            let note = container.lastChild;
+            note.append(html_for_name_of_note);
+
+            note.lastChild.textContent = nameOfNotification;
+            note.append(html_for_note);
+            note.lastChild.textContent = textOfNotification;
+
         }else{
-            container.appendChild(containerForNotifition);
-            let note = container.lastChild
-            note.append('<p class="text-of-note>"' + textOfNotification + '</p>')
+            container.append(containerForNotification);
+            let note = container.lastChild;
+            note.append(html_for_note);
+            note.lastChild.textContent = textOfNotification;
+
         }
     }
 }
